b4d67d0275f21de3acdf82452ff251ccc06a08de
Ad Friedman <adam.friedman@flatironschool.com>
2019-04-21 15:55:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Den"
+  end
+
+  get '/hometown' do
+    "My hometown is Arcaidia"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'La'"
+  end
+
 end
