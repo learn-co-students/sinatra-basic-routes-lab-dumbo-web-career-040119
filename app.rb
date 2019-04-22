c8ac9b2497d08e0dbe9fbe33086945a866c959c1
Mallory Woods <malvwoods@gmail.com>
2019-04-22 10:14:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Mallory"
+  end
+
+  get '/hometown' do
+    "My hometown is Littleton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Star Spangled Banner"
+  end
+
 end
