fdb1ddc7e7508682241a737a370ae61b607b7110
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-04-22 00:16:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sylwia."
+  end
+
+  get '/hometown' do
+    "My hometown is Zielona Góra and New York."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Rigamortis."
+  end
 end
