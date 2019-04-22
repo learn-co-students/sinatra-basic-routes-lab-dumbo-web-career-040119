403988a422b61a4a506a08b1f6299269d57d9347
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-22 17:49:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Luka"
+  end 
+  
+  get '/hometown' do
+    "My hometown is Tbilisi"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is wht"
+  end 
 end
